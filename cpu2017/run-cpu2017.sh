5389a18b1160106ba6d4bbd8806794f4feaa3df0
SRB <1084429107@qq.com>
2022-11-11 18:43:13 +0800

    debug1

--- cpu2017/run-cpu2017.sh
+++ cpu2017/run-cpu2017.sh
@@ -8,7 +8,7 @@
 # Change the following global variables based on your environment
 #-------------------------------------------------------------------------------
 EMON="/opt/intel/oneapi/vtune/2021.5.0/bin64/emon" # Emon path
-RUNDIR="~/Pond" # top dir of this repo 为什么不直接source cxl-globols.sh呢，这样就有top了啊
+RUNDIR="/home/wangkunyun/Pond" # top dir of this repo 为什么不直接source cxl-globols.sh呢，这样就有top了啊
 
 # Output folder
 #RSTDIR="rst/emon-$(date +%F-%H%M)-$(uname -n | awk -F. '{printf("%s.%s\n", $1, $2)}')"
